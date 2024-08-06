c379583be22063257adfbf8d7310cc39f3e1cccc
paulodesiqueira <paulodesiqueira@gmail.com>
2024-08-06 18:29:09 -0300

    Equals and HashCode

--- dscommerce/src/main/java/com/siqueyros/dscommerce/entities/OrderItem.java
+++ dscommerce/src/main/java/com/siqueyros/dscommerce/entities/OrderItem.java
@@ -2,6 +2,8 @@ package com.siqueyros.dscommerce.entities;
 
 import jakarta.persistence.*;
 
+import java.util.Objects;
+
 @Entity
 @Table(name = "tb_order_item")
 public class OrderItem {
@@ -51,5 +53,19 @@ public class OrderItem {
 
     public void setPrice(Double price) {
         this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        OrderItem orderItem = (OrderItem) o;
+        return Objects.equals(id, orderItem.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
     }
 }
